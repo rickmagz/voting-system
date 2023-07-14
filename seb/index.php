7320c5873fb02a4840701fc038274724cf192375
rickmagz <ramnashor2@gmail.com>
2023-07-14 08:16:11 +0800

    Added SEB Login

--- seb/index.php
+++ seb/index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+include '../db.php';
 
 ?>
 <!DOCTYPE html>
@@ -42,16 +44,16 @@
               <div>
                 <img src="../assets/img/ISAT-U-logo-shadow1.png" style="width: 100px; height: 100px" /><img src="../assets/img/sr-logo.png" style="width: 100px; height: 100px" /><span style="font-size: 24px; color: var(--bs-blue)"><br /><strong>ISATU - Miagao Campus</strong><br /><strong>Student Electoral Commission Dashboard</strong><br /></span>
               </div>
-              <form class="text-center" action="index.php" method="post" style="margin: 5px">
+              <form class="text-center" action="index.php" method="post" style="margin: 5px" id="login">
                 <div class="row" style="padding-top: 28px; padding-bottom: 28px">
                   <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 offset-sm-2 offset-md-2 offset-lg-2 offset-xl-2" style="padding-bottom: 12px">
-                    <input class="form-control" type="email" name="email" placeholder="Email" autofocus="" required="" />
+                    <input class="form-control" type="text" name="username" placeholder="Enter username" autofocus="" required="" />
                   </div>
                   <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 offset-sm-2 offset-md-2 offset-lg-2 offset-xl-2">
-                    <input class="form-control" type="password" name="password" placeholder="Password" />
+                    <input class="form-control" type="password" name="password" placeholder="Enter Password" />
                   </div>
                   <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 offset-sm-3 offset-md-3 offset-lg-3 offset-xl-3 align-self-center" style="padding-top: 15px">
-                    <button class="btn btn-primary d-block w-100" type="submit">
+                    <button class="btn btn-primary d-block w-100" type="submit" name="login" form="login">
                       Login
                     </button>
                   </div>
@@ -67,6 +69,26 @@
       </div>
     </div>
   </header>
+  <?php
+  if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
+
+    $login = mysqli_query($cxn, "SELECT * FROM seb WHERE username='$username' AND password='$pass'");
+
+    if (mysqli_num_rows($login) > 0) {
+      $l = mysqli_fetch_assoc($login);
+      $_SESSION['username'] = $l['username'];
+      $_SESSION['student_id'] = $l['student_id'];
+      $_SESSION['first_name'] = $l['first_name'];
+      $_SESSION['last_name'] = $l['last_name'];
+      header("location: home.php");
+    } else {
+      echo '<script type="text/javascript"> alert("Invalid Credentials!")</script>';
+    }
+  }
+  ?>
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="../assets/js/bs-init.js"></script>
